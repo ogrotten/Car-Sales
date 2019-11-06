d6008acb0e371f8661eb8280f755166a487c53d9
Darrin Lowery <lambda@monsto.com>
2019-11-06 14:25:38 -0600

    initial commit plus

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+// temp to see the page again, remv later 
+// temp to see the page again, remv later 
+// temp to see the page again, remv later 
+
+const reducer = () => {};
+const initialState = ""
+
+const store = createStore(reducer, initialState);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+);
